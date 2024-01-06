d1746de6fbe5b8ef7592d8d93aae2b9c1c3e5c20
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-01-06 11:46:36 +0530

    Update settings.py

--- mychat/settings.py
+++ mychat/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-iz)lu!69b#vt$0*np6x))5!%2o+kk7e)o*883u!-rfg=iq1bn_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
