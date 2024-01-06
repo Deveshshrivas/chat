91da5c976c343fcef54b4d62fe17ee53271ea62d
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-01-06 19:07:00 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management import call_command
 
 
 def main():
@@ -15,6 +16,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    execute_from_command_line(sys.argv)
+    call_command('collectstatic', '--noinput')
     execute_from_command_line(sys.argv)
 
 
